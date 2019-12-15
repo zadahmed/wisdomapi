7dfc3d85407e4d3313082a596b3611b0ec783dc6
Zahid Ahmed <zahidahmed@Zahids-MacBook-Pro.local>
2019-12-15 21:36:28 +0000

    APIv1

--- MontGomery/BaseMonty.py
+++ MontGomery/BaseMonty.py
@@ -83,7 +83,7 @@ class BaseMonty():
             }
         )
         options.binary_location = GOOGLE_CHROME_PATH
-        driver = webdriver.Chrome(execution_path=CHROMEDRIVER_PATH,options=options)
+        driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH,chrome_options=options)
         max_screen_width = driver.get_window_size().get('width')
         max_screen_height = driver.get_window_size().get('height')
         return start_datetime, start, driver, max_screen_width, max_screen_height
