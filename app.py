865069a8d620f0b0cd8efe8d9d716bf8ebd7b1c3
zahidahmed <zadahmed@outlook.com>
2019-12-16 12:50:26 +0000

    APIv1.

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ app = Flask(__name__)
 dir = os.path.abspath(os.path.dirname(__file__)) #  Directory
 
 # Start
-start_datetime,  start, driver, max_screen_width, max_screen_height = s.liftOff()
+# start_datetime,  start, driver, max_screen_width, max_screen_height = s.liftOff()
 
 
 # @app.route('/search',methods = ['GET'])
@@ -55,83 +55,83 @@ def search(search_me,search_topic , relevance,summary_points):
             print("~"*50, " Wisdom ", "~"*50)
             jsonobject = s.factualSummary(search_me, summary_points, f_what_summary)
             return jsonobject
-        elif search_topic=="Topical":
-            # Topical
-            t.youtubeFilter(search_me, driver, max_screen_width, max_screen_height)
-            #t.subtitlesFilter(driver, max_screen_width, max_screen_height)
-            t_video_names, t_to_watch, t_video_urls = t.getVideos(search_me, driver, relevance)
-            t_video_ids, t_video_dates, t_video_locations = t.watchVideos(driver, max_screen_width, t_video_urls)
-            t_video_corpus, t_video_entities = t.montyWatchingTime()
-            t_related_searches, t_article_links, t_news_articles, t_news_dates, t_news_entities, t_news_corpus, t_news_locations = t.montyReadingTime(search_me, driver, relevance)
-            print("\n")
-            print("~"*50, " Wisdom ", "~"*50)
-            print("\n")
-            jsonobject = s.topicalSummary(search_me, t_news_corpus, t_video_corpus, summary_points)    
-            # Appendix
-            print("\n")
-            print("~"*50, " Wisdom ", "~"*50)
-            s.topicalAppendix(t_video_ids, t_video_urls, t_video_dates, t_news_articles, t_news_dates, t_article_links)
-            return jsonobject
-        elif search_topic=="Influential":
-            # Influential
-            i_blogs_links, i_blogs_articles, i_blogs_dates = i.goGoogleBlogs(search_me, driver, relevance)
-            i_blogs_corpus, i_blogs_entities = i.extractGoogleBlogs(i_blogs_links, i_blogs_articles)
-            i.goMedium(search_me, driver)
-            i_medium_links = i.getMedium(search_me, driver, relevance)
-            i_medium_corpus, i_medium_entities, i_medium_authors, i_medium_dates = i.extractMedium(driver, i_medium_links, max_screen_width)
-            i_blogs_corpus, i_blogs_entities, i_blogs_links, i_blogs_authors, i_blogs_dates = i.blogsMerge(i_blogs_corpus, i_blogs_entities, i_blogs_links, i_medium_corpus, i_medium_links, i_medium_entities, i_medium_authors, i_medium_dates)
-            i_elsevier_titles, i_elsevier_links, i_elsevier_authors = i.goElsevier(search_me, driver)
-            i_elsevier_abstracts = i.extractElsevier(i_elsevier_titles, i_elsevier_links, driver)
-            i_arxiv_links, i_arxiv_titles = i.goArXiv(search_me, driver)
-            i_arxiv_abstracts, i_arxiv_authors = i.extractArxiv(i_arxiv_links, i_arxiv_titles)
-            i_research_titles, i_research_links, i_research_authors, i_research_abstracts = i.researchMerge(i_elsevier_titles, i_elsevier_links, i_elsevier_authors, i_elsevier_abstracts, i_arxiv_links, i_arxiv_titles, i_arxiv_abstracts, i_arxiv_authors)
-            print("~"*50, " Wisdom ", "~"*50)
-            jsonobject = s.influentialSummary(search_me, summary_points, i_blogs_corpus, i_blogs_authors, i_research_abstracts, i_research_authors)
-            # Appendix
-            print("~"*50, " Wisdom ", "~"*50)
-            s.influentialAppendix(i_blogs_links, i_research_titles, i_research_authors)
-            return jsonobject
-        elif search_topic=="Everything":
-            # Factual
-            f.goWikipediaWhat(search_me, driver)
-            f_wiki_soup = f.getWikipediaWhat(driver)
-            f.goWikipediaWhen(search_me, driver)
-            f_history_soup, f_history_type = f.getWikipediaWhen(driver, search_me)
-            f_what_summary, f_old_history_summary_sorted, f_history_summary_sorted = f.wikiSummary(f_wiki_soup, f_history_soup, f_history_type, search_me)
-            # Topical
-            t.youtubeFilter(search_me, driver, max_screen_width, max_screen_height)
-            #t.subtitlesFilter(driver, max_screen_width, max_screen_height)
-            t_video_names, t_to_watch, t_video_urls = t.getVideos(search_me, driver, relevance)
-            t_video_ids, t_video_dates, t_video_locations = t.watchVideos(driver, max_screen_width, t_video_urls)
-            t_video_corpus, t_video_entities = t.montyWatchingTime()
-            t_related_searches, t_article_links, t_news_articles, t_news_dates, t_news_entities, t_news_corpus, t_news_locations = t.montyReadingTime(search_me, driver, relevance)
-            # Influential
-            i_blogs_links, i_blogs_articles, i_blogs_dates = i.goGoogleBlogs(search_me, driver, relevance)
-            i_blogs_corpus, i_blogs_entities = i.extractGoogleBlogs(i_blogs_links, i_blogs_articles)
-            i.goMedium(search_me, driver)
-            i_medium_links = i.getMedium(search_me, driver, relevance)
-            i_medium_corpus, i_medium_entities, i_medium_authors, i_medium_dates = i.extractMedium(driver, i_medium_links, max_screen_width)
-            i_blogs_corpus, i_blogs_entities, i_blogs_links, i_blogs_authors, i_blogs_dates = i.blogsMerge(i_blogs_corpus, i_blogs_entities, i_blogs_links, i_medium_corpus, i_medium_links, i_medium_entities, i_medium_authors, i_medium_dates)
-            i_elsevier_titles, i_elsevier_links, i_elsevier_authors = i.goElsevier(search_me, driver)
-            i_elsevier_abstracts = i.extractElsevier(i_elsevier_titles, i_elsevier_links, driver)
-            i_arxiv_links, i_arxiv_titles = i.goArXiv(search_me, driver)
-            i_arxiv_abstracts, i_arxiv_authors = i.extractArxiv(i_arxiv_links, i_arxiv_titles)
-            i_research_titles, i_research_links, i_research_authors, i_research_abstracts = i.researchMerge(i_elsevier_titles, i_elsevier_links, i_elsevier_authors, i_elsevier_abstracts, i_arxiv_links, i_arxiv_titles, i_arxiv_abstracts, i_arxiv_authors)
-            # Summary
-            print("~"*50, " Wisdom ", "~"*50)
-            s.factualSummary(search_me, summary_points, f_what_summary, f_old_history_summary_sorted, f_history_summary_sorted)
-            s.topicalSummary(search_me, t_news_corpus, t_video_corpus, summary_points)
-            s.influentialSummary(search_me, summary_points, i_blogs_corpus, i_blogs_authors, i_research_abstracts, i_research_authors)
-            # Appendix
-            print("~"*50, " Wisdom ", "~"*50)
-            s.contextSummary(t_news_entities, t_video_entities, t_video_dates, i_blogs_entities, i_blogs_dates, t_news_dates, t_news_locations, t_video_locations, t_related_searches, search_me)
-            print("~"*50, " Wisdom ", "~"*50)
-            s.topicalAppendix(t_video_ids, t_video_urls, t_video_dates, t_news_articles, t_news_dates, t_article_links)
-            s.influentialAppendix(i_blogs_links, i_research_titles, i_research_authors)
+        # elif search_topic=="Topical":
+        #     # Topical
+        #     t.youtubeFilter(search_me, driver, max_screen_width, max_screen_height)
+        #     #t.subtitlesFilter(driver, max_screen_width, max_screen_height)
+        #     t_video_names, t_to_watch, t_video_urls = t.getVideos(search_me, driver, relevance)
+        #     t_video_ids, t_video_dates, t_video_locations = t.watchVideos(driver, max_screen_width, t_video_urls)
+        #     t_video_corpus, t_video_entities = t.montyWatchingTime()
+        #     t_related_searches, t_article_links, t_news_articles, t_news_dates, t_news_entities, t_news_corpus, t_news_locations = t.montyReadingTime(search_me, driver, relevance)
+        #     print("\n")
+        #     print("~"*50, " Wisdom ", "~"*50)
+        #     print("\n")
+        #     jsonobject = s.topicalSummary(search_me, t_news_corpus, t_video_corpus, summary_points)    
+        #     # Appendix
+        #     print("\n")
+        #     print("~"*50, " Wisdom ", "~"*50)
+        #     s.topicalAppendix(t_video_ids, t_video_urls, t_video_dates, t_news_articles, t_news_dates, t_article_links)
+        #     return jsonobject
+        # elif search_topic=="Influential":
+        #     # Influential
+        #     i_blogs_links, i_blogs_articles, i_blogs_dates = i.goGoogleBlogs(search_me, driver, relevance)
+        #     i_blogs_corpus, i_blogs_entities = i.extractGoogleBlogs(i_blogs_links, i_blogs_articles)
+        #     i.goMedium(search_me, driver)
+        #     i_medium_links = i.getMedium(search_me, driver, relevance)
+        #     i_medium_corpus, i_medium_entities, i_medium_authors, i_medium_dates = i.extractMedium(driver, i_medium_links, max_screen_width)
+        #     i_blogs_corpus, i_blogs_entities, i_blogs_links, i_blogs_authors, i_blogs_dates = i.blogsMerge(i_blogs_corpus, i_blogs_entities, i_blogs_links, i_medium_corpus, i_medium_links, i_medium_entities, i_medium_authors, i_medium_dates)
+        #     i_elsevier_titles, i_elsevier_links, i_elsevier_authors = i.goElsevier(search_me, driver)
+        #     i_elsevier_abstracts = i.extractElsevier(i_elsevier_titles, i_elsevier_links, driver)
+        #     i_arxiv_links, i_arxiv_titles = i.goArXiv(search_me, driver)
+        #     i_arxiv_abstracts, i_arxiv_authors = i.extractArxiv(i_arxiv_links, i_arxiv_titles)
+        #     i_research_titles, i_research_links, i_research_authors, i_research_abstracts = i.researchMerge(i_elsevier_titles, i_elsevier_links, i_elsevier_authors, i_elsevier_abstracts, i_arxiv_links, i_arxiv_titles, i_arxiv_abstracts, i_arxiv_authors)
+        #     print("~"*50, " Wisdom ", "~"*50)
+        #     jsonobject = s.influentialSummary(search_me, summary_points, i_blogs_corpus, i_blogs_authors, i_research_abstracts, i_research_authors)
+        #     # Appendix
+        #     print("~"*50, " Wisdom ", "~"*50)
+        #     s.influentialAppendix(i_blogs_links, i_research_titles, i_research_authors)
+        #     return jsonobject
+        # elif search_topic=="Everything":
+        #     # Factual
+        #     f.goWikipediaWhat(search_me, driver)
+        #     f_wiki_soup = f.getWikipediaWhat(driver)
+        #     f.goWikipediaWhen(search_me, driver)
+        #     f_history_soup, f_history_type = f.getWikipediaWhen(driver, search_me)
+        #     f_what_summary, f_old_history_summary_sorted, f_history_summary_sorted = f.wikiSummary(f_wiki_soup, f_history_soup, f_history_type, search_me)
+        #     # Topical
+        #     t.youtubeFilter(search_me, driver, max_screen_width, max_screen_height)
+        #     #t.subtitlesFilter(driver, max_screen_width, max_screen_height)
+        #     t_video_names, t_to_watch, t_video_urls = t.getVideos(search_me, driver, relevance)
+        #     t_video_ids, t_video_dates, t_video_locations = t.watchVideos(driver, max_screen_width, t_video_urls)
+        #     t_video_corpus, t_video_entities = t.montyWatchingTime()
+        #     t_related_searches, t_article_links, t_news_articles, t_news_dates, t_news_entities, t_news_corpus, t_news_locations = t.montyReadingTime(search_me, driver, relevance)
+        #     # Influential
+        #     i_blogs_links, i_blogs_articles, i_blogs_dates = i.goGoogleBlogs(search_me, driver, relevance)
+        #     i_blogs_corpus, i_blogs_entities = i.extractGoogleBlogs(i_blogs_links, i_blogs_articles)
+        #     i.goMedium(search_me, driver)
+        #     i_medium_links = i.getMedium(search_me, driver, relevance)
+        #     i_medium_corpus, i_medium_entities, i_medium_authors, i_medium_dates = i.extractMedium(driver, i_medium_links, max_screen_width)
+        #     i_blogs_corpus, i_blogs_entities, i_blogs_links, i_blogs_authors, i_blogs_dates = i.blogsMerge(i_blogs_corpus, i_blogs_entities, i_blogs_links, i_medium_corpus, i_medium_links, i_medium_entities, i_medium_authors, i_medium_dates)
+        #     i_elsevier_titles, i_elsevier_links, i_elsevier_authors = i.goElsevier(search_me, driver)
+        #     i_elsevier_abstracts = i.extractElsevier(i_elsevier_titles, i_elsevier_links, driver)
+        #     i_arxiv_links, i_arxiv_titles = i.goArXiv(search_me, driver)
+        #     i_arxiv_abstracts, i_arxiv_authors = i.extractArxiv(i_arxiv_links, i_arxiv_titles)
+        #     i_research_titles, i_research_links, i_research_authors, i_research_abstracts = i.researchMerge(i_elsevier_titles, i_elsevier_links, i_elsevier_authors, i_elsevier_abstracts, i_arxiv_links, i_arxiv_titles, i_arxiv_abstracts, i_arxiv_authors)
+        #     # Summary
+        #     print("~"*50, " Wisdom ", "~"*50)
+        #     s.factualSummary(search_me, summary_points, f_what_summary, f_old_history_summary_sorted, f_history_summary_sorted)
+        #     s.topicalSummary(search_me, t_news_corpus, t_video_corpus, summary_points)
+        #     s.influentialSummary(search_me, summary_points, i_blogs_corpus, i_blogs_authors, i_research_abstracts, i_research_authors)
+        #     # Appendix
+        #     print("~"*50, " Wisdom ", "~"*50)
+        #     s.contextSummary(t_news_entities, t_video_entities, t_video_dates, i_blogs_entities, i_blogs_dates, t_news_dates, t_news_locations, t_video_locations, t_related_searches, search_me)
+        #     print("~"*50, " Wisdom ", "~"*50)
+        #     s.topicalAppendix(t_video_ids, t_video_urls, t_video_dates, t_news_articles, t_news_dates, t_article_links)
+        #     s.influentialAppendix(i_blogs_links, i_research_titles, i_research_authors)
         else:
             print("- Search topic error...")
         # Finish
-        s.finish(driver, start_datetime, start, search_topic, search_me, summary_points)
+        # s.finish(driver, start_datetime, start, search_topic, search_me, summary_points)
     except Exception as e:
         print("\n~~~~> Monty had to exit... visit again soon! :)")
         print("\n Error received:")
@@ -148,7 +148,7 @@ def search(search_me,search_topic , relevance,summary_points):
                 clean_up.append(file)
         for file in clean_up:
             os.remove(file)
-        driver.close()
+        # driver.close()
 
 #Run Server
 if __name__ == '__main__':
