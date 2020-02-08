db63b9f9f31b1be6ba3f60122c9e8914e5b2f9d3
Arun Godwin Patel <arungodwinpatel@googlemail.com>
2020-02-08 15:52:40 +0000

    feat: added basic CRUD to mongodb from flask routes

--- app.py
+++ app.py
@@ -5,34 +5,90 @@ from flask import Flask , request , jsonify
 import os
 import wisdomaiengine
 from urllib.parse import unquote
-
+import pymongo
+import sys
 
 app = Flask(__name__)
 dir = os.path.abspath(os.path.dirname(__file__)) #  Directory
 
+# user and pwd need to be environment variable os.environ["MONGO_DB_USER"]
+user = "admin"
+pwd = "admin123"
 
+# connect to db
+try:
+    client = pymongo.MongoClient("mongodb://"+user+":"+pwd+"@localhost:27017/")
+    db = client["wisdomdb"]
+    print("connected to mongodb successfully!")
+except:
+    print("Unable to conenct to database")
+    sys.exit(1)
 
+# instantiate collections
+try:
+    users = db["users"]
+    searches = db["searches"]
+    search_terms = db["search_terms"]
+    byod = db["byod"]
+    highlights = db["highlights"]
+    wikipedia = db["wikipedia"]
+    arxiv = db["arxiv"]
+except:
+    print("Unable to connect to collections")
+    sys.exit(1)
+    
+# routes
 @app.route('/',methods = ['GET'])
 def home():
     return jsonify({'msg':'Hello World'})
 
-@app.route('/wisdom/<path:pdfurl>',methods=['GET'])
-def wisdom(pdfurl):
+@app.route('/wisdom/abstract/<path:pdfurl>',methods=['GET'])
+def wisdomabstract(pdfurl ):
+    print(pdfurl)
     text = wisdomaiengine.pdfdocumentextracter(pdfurl)
-    summary = wisdomaiengine.summarisepdfdocument(text)
-    topics = wisdomaiengine.topicsindocument(text)
-    print(text)
-    print(summary)
+    abstract = wisdomaiengine.abstractextracter(pdfurl)
+    topics = wisdomaiengine.wordcloud( text)
+    if topics is None:
+        topics = ['No Topics Found']
+    print(abstract)
     print(topics)
-    summaryjson = jsonify(wisdomsummary = summary , wisdomtopics = topics)
+    # write data top arxiv collection
+    data = {"url": pdfurl, "text": text, "abstract": abstract, "topics": topics}
+    x = arxiv.insert_one(data)
+    summaryjson = jsonify(wisdomtopics = topics , wisdomabstract = abstract)
     return summaryjson
 
-@app.route('/search/<string:search_me>/<string:search_topic>/<int:relevance>/<int:summary_points>',methods = ['GET'])
+
+@app.route('/wisdom/summary/<path:pdfurl>',methods=['GET'])
+def wisdomsummary(pdfurl ):
+    print(pdfurl)
+    text = wisdomaiengine.pdfdocumentextracter(pdfurl)
+    summary = wisdomaiengine.summarisepdfdocument(text)
+    print(summary)
+    summaryjson = jsonify(wisdomsummary = summary )
+    # write data to arxiv collection
+    data = {"url": pdfurl, "text": text, "summary": summary}
+    x = arxiv.insert_one(data)
+    return summaryjson
+
+
+# @app.route('/wisdom/<path:pdfurl>',methods=['GET'])
+# def wisdombyod(pdfurl ):
+#     summary = wisdomaiengine.summarisepdfdocument(text)
+#     print(summary)
+#     summaryjson = jsonify(wisdomsummary = summary )
+#     return summaryjson
+
+
+@app.route('/search/<string:search_me>/<string:search_topic>/<int:relevance>/<int:summary_points>',methods = ['GET'$
 def search(search_me,search_topic , relevance,summary_points):
 # Run
     try:
         if search_topic=="Factual":
             page = wptools.page(search_me.lower())
+            # write data to search_terms collection
+            data = {"value": search_me}
+            x = search_terms.insert_one(data)
             r = page.get()
             f_what_summary = r.data['extext']
 
@@ -47,11 +103,12 @@ def search(search_me,search_topic , relevance,summary_points):
             max_chunk_results=10)
             papers = []
             wordpapers = []
-
+            
             for paper in result:
                 title = paper['summary']
                 title = title.replace('\n', ' ')
                 value = paper['title_detail']['value']
+                value = value.replace('\n', ' ')
                 pdf_url = paper['pdf_url']
                 print('\nSummary ' + title)
                 print('\nTitle ' + value)
@@ -59,9 +116,13 @@ def search(search_me,search_topic , relevance,summary_points):
                 papers.append([title,value,pdf_url])
                 wordpapers.append([title])
             # jsonobject = s.factualSummary(search_me, summary_points, f_what_summary)
-            # wordcloud = wisdomaiengine.wordcloud(search_me,wordpapers)
+            wordcloud = wisdomaiengine.wordcloud(wordpapers)
+            print(wordcloud)
             print(wordpapers)
-            jsonob = jsonify(search= search_me , summary= f_what_summary , papers= papers )
+            # write data to searches collection
+            data = {"search_id": search_me}
+            x = searches.insert_one(data)
+            jsonob = jsonify(search= search_me , summary= f_what_summary , papers= papers ,wordcloud = wordcloud )
             return jsonob
         else:
             print("- Search topic error...")
@@ -83,6 +144,8 @@ def search(search_me,search_topic , relevance,summary_points):
             os.remove(file)
         # driver.close()
 
-#Run Server
+# run server
 if __name__ == '__main__':
-    app.run(threaded=True, port=5000)
+    app.run(host='0.0.0.0',threaded=True, port=5000)
+                                                                                                              
+                                                                                                              
